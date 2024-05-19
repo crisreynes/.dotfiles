b27678c75fa1cb0b29d9c8218ad97521727e71a0
Cristian Reynés <cristianreynes@gmail.com>
2024-05-19 19:09:46 -0300

    Reorder execution and modify xrandr command

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-picom -b &
 warpd &
 nm-applet &
 feh --randomize --bg-fill ~/Pictures/wallpapers/pc/current/*
 setxkbmap -model pc105 -layout us
-# xrandr --output DisplayPort-2 --mode 1920x1080 --rate 144
+# xrandr --rate 144
+picom -b &
